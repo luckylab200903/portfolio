a3949fa2ea5d8c5e6e54811f9f01de5262c673e2
keshav kumar singh <kumarkeshavsingh200903@gmail.com>
2025-07-04 15:55:09 +0530

    added file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
+
 import { Toaster } from "@/components/ui/toaster";
+import { NotFound } from "./pages/Notfound";
 
 function App() {
   return (
